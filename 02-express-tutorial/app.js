ed30c1ba1bfc58c22d52d1c42a8bd23870e5ebcd
vijaya2497 <sweetyvijaya06@gmail.com>
2023-01-05 11:56:57 +0530

    first commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,40 @@
-console.log('Express Tutorial')
+const express = require('express')
+const app = express()
+
+const {products} = require('./data.js')
+
+
+app.get('/', (req,res)=>{
+    res.send('<h1> Home Page </h1><a href="/api/products">products</a>')
+    })
+
+app.get('/api/products', (req,res)=>{
+   const newProduct = products.map((product) => {
+    const {id,name,image} = product
+    return {id,name,image}
+
+   })
+   res.json(newProduct)
+    })
+
+app.get('/api/products/:productID', (req,res)=>{
+    //console.log(req)
+    //console.log(req.params)
+    const {productID} = req.params
+    const singleProduct = products.find(
+        (product) => product.id === Number(productID)
+        )
+    if(!(singleProduct))
+    {
+        return res.status(404).send('Product doesnt exists')
+    }
+    else {
+        return res.json(singleProduct)
+    }
+})
+
+app.listen(5000, ()=>{
+    console.log("server listening on port 5000...")
+})
+
+
